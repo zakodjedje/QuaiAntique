6490fa85ba49ad8a02c77f7ecc6fcd3c1e01cde0
djedje <zakodjedje@gmail.com>
2024-10-10 18:37:26 +0200

    page account et editpassword

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,6 +5,8 @@ export const allRoutes = [
     new Route("/galerie", "galerie","/pages/galerie.html"),
     new Route("/signin", "Connexion","/pages/signin.html"),
     new Route("/signup", "Inscription","/pages/signup.html"),
+    new Route("/account", "Mon compte","/pages/account.html"),
+    new Route("/editPassword", "changement de mot passe","/pages/editPassword.html"),
 ]
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
