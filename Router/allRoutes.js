4208a4590b866895086323c58512bc31aa47aee8
djedje <zakodjedje@gmail.com>
2024-10-10 16:39:54 +0200

    ajout page signup

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,6 +4,7 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "galerie","/pages/galerie.html"),
     new Route("/signin", "Connexion","/pages/signin.html"),
+    new Route("/signup", "Inscription","/pages/signup.html"),
 ]
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
