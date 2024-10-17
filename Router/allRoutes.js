b97ee24777a581236de209adc3ee113a5b29b20d
djedje <zakodjedje@gmail.com>
2024-10-17 20:26:37 +0200

    page signin.js + script.js , gestion du cookie

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,7 +3,7 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "galerie","/pages/galerie.html"),
-    new Route("/signin", "Connexion","/pages/signin.html"),
+    new Route("/signin", "Connexion","/pages/signin.html", "/js/auth/signin.js"),
     new Route("/signup", "Inscription","/pages/signup.html", "/js/auth/signup.js"),
     new Route("/account", "Mon compte","/pages/account.html"),
     new Route("/editPassword", "changement de mot passe","/pages/editPassword.html"),
---
+++ js/auth/signin.js
@@ -0,0 +1,24 @@
+
+
+const mailInput = document.getElementById("EmailInput");
+const passwordInput = document.getElementById ("PasswordInput");
+const btnSignin = document.getElementById("btnSignin");
+
+btnSignin.addEventListener ("click", checkCredentials);
+
+function checkCredentials (){
+
+    if (mailInput.value == "test@mail.com" && passwordInput.value == "123"){
+        alert("vous etes connecté");
+
+        const token = "abcdefgh "
+        setToken (token);
+        window.location.replace("/");
+    }
+    else {
+        mailInput.classList.add("is-invalid");
+        passwordInput.classList.add("is-invalid")
+    }
+
+
+}
